eb08e0c720d16ec7a285b29b6e7527a83041f24f
Kishan Maisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-07-28 10:13:43 +1200

    New test cases

---
+++ Auto test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import unittest
+
+
+
+def test_read_csv():
+    columns = [0,4]
+    filepath = 'C:\\Users\\sskis\\OneDrive\\Desktop\\Python stuff T3\\Python-T3\\DiamondValues(1000).xlsx'
+    dataframe = pd.read_csv(filepath,columns)
+    assert not dataframe.empty,'CSV data should not be empty'
+    excepted_columns = ['Carat Weight','Price']
+    assert all (col in dataframe.columns for col in excepted_columns),'worky'
+    
+    
