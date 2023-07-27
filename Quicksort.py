ae5ac6c91997cb80941db828c1aeb544dc5461fc
Kishan Maisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-07-27 16:09:47 +1200

    trtail

--- Quicksort.py
+++ Quicksort.py
@@ -8,25 +8,25 @@ def Qsort (value):
         pivot = value[0]
         left = [x for x in value[1:]if x < pivot]
         right = [x for x in value[1:]if x > pivot]
-        return Qsort(left)+[pivot]+ Qsort(right)
+        return Qsort(left) + [pivot] + Qsort(right)
     
-def read_excel(filepath, column):
-    dataframe1 = pd.read_excel(filepath,usecols = column)
-    return dataframe1
+#def read_excel(filepath, column):
+ #   dataframe1 = pd.read_excel(filepath)
+  #  return dataframe1
 
 
 
          
-column = [0,4]
+column = 'Price'
 filepath = 'C:\\Users\\sskis\\OneDrive\\Desktop\\Python stuff T3\\Python-T3\\DiamondValues(1000).xlsx'
-dataframe1 = read_excel(filepath, column)
+dataframe1 = pd.read_excel(filepath)
 
 print("Original Data:")
 print(dataframe1)
 
 
 
-data_list = dataframe1.to_dict('records')
+data_list = dataframe1[column].tolist()
 
 
 Qsort(data_list)
